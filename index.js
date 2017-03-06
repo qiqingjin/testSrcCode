e83a8e093ceb8a64a2b93ae94b3f90a76419c703
liyueyang <liyueyang1991@163.com>
2017-03-06 15:47:02 +0800

    测试使用require打包文件

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Cynthia
+* @Date:   2017-03-06 14:51:16
+* @Last Modified by:   Cynthia
+* @Last Modified time: 2017-03-06 15:02:55
+*/
+
+'use strict';
+
+require('./src/bundle_require.js');
---
+++ src/bundle_require.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Claire
+* @Date:   2017-03-06 13:23:38
+* @Last Modified by:   Cynthia
+* @Last Modified time: 2017-03-06 15:03:15
+*/
+
+'use strict';
+
+/*(function(a){
+	console.log(a.name);
+})
+({name: 'hello'})*/
+
+console.log('success');
